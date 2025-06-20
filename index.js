dcead17ef851754d309dc38623fb943a528bbf99
cartieraidan <cartieraidan@gmail.com>
2025-06-20 16:30:45 -0400

    Update index.js
    Adding a way to send native messaging for testing

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ document.getElementById("myButton").addEventListener("click", () => {
     
     chrome.runtime.sendMessage({ type: "log", message: "button has been pressed" });
 
+    chrome.runtime.sendNativeMessage(
+        "com.example.script",
+        { text: "Hello Python" },
+        (response) => {
+            chrome.runtime.sendMessage({ type: "log", message: "Python said: ", response });
+        }
+    );
+
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
