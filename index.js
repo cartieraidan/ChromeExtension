e9de77801ede49121e6e25bb5236dde46a354d99
cartieraidan <cartieraidan@gmail.com>
2025-06-23 11:58:00 -0400

    Update index.js
    No longer using native messaging

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ document.getElementById("myButton").addEventListener("click", () => {
     
     chrome.runtime.sendMessage({ type: "log", message: "button has been pressed" });
 
-    chrome.runtime.sendNativeMessage(
-        "com.example.script",
-        { text: "Hello Python" },
-        (response) => {
-            chrome.runtime.sendMessage({ type: "log", message: "Python said: ", response });
-        }
-    );
 
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
