5c201058f2534c0f4df710d1503d4de73ff7a8e1
cartieraidan <cartieraidan@gmail.com>
2025-07-02 14:10:32 -0400

    Update injectTest.js
    Handshake for verifying communication

--- injectTest.js
+++ injectTest.js
@@ -1,8 +1,29 @@
 (function () {
+    
+    let count = 0;
+    const maxCount = 10000;
+    
+     window.addEventListener("message", function(event) {
+        if (event.source !== window) return;
 
-   setInterval(() => {
-        var data = { type: "FROM_PAGE", text: "Message from webpage"};
-   window.postMessage(data, "*");
-   }, 1000); 
-   //look into doing handshakes
+        if (event.data.type === "extension-ready") {
+            var data = { type: "FROM_PAGE", text: "Connected"};
+            window.postMessage(data, "*");
+            clearInterval(interval);
+        }
+    });
+
+    const interval = setInterval(() => {
+        
+        window.postMessage({ type: "page-ready"}, "*");
+        
+        count += 1000;
+        if (count >= 10000) {
+           // window.postMessage({ type: "extension-ready"}, "*");
+            clearInterval(interval);
+        }
+    
+    }, 1000); 
+   
+
 })();
