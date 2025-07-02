c75e30b142b654e6d869be028094f5dfe3179867
cartieraidan <cartieraidan@gmail.com>
2025-07-02 12:27:07 -0400

    Update injectTest.js
    Testing communication

--- injectTest.js
+++ injectTest.js
@@ -1,3 +1,8 @@
 (function () {
-   
+
+   setInterval(() => {
+        var data = { type: "FROM_PAGE", text: "Message from webpage"};
+   window.postMessage(data, "*");
+   }, 1000); 
+   //look into doing handshakes
 })();
