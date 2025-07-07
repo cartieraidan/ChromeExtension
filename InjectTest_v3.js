209f54fe0db3a054dd7f5b7ed73a0e6a7885c46d
cartieraidan <cartieraidan@gmail.com>
2025-07-07 10:44:17 -0400

    Add files via upload

---
+++ InjectTest_v3.js
@@ -0,0 +1,52 @@
+(function () {
+    
+    let count = 0;
+    const maxCount = 10000;
+    let conected = false;
+    
+    window.addEventListener("message", function(event) {
+        if (event.source !== window) return;
+
+        if (event.data.type === "extension-ready" && !conected) {
+            var data = { type: "FROM_PAGE", text: "Connected"};
+            window.postMessage(data, "*");
+
+            conected = true;
+
+            clearInterval(interval);
+            afterHandshake();
+        }
+    });
+
+    const interval = setInterval(() => {
+        
+        window.postMessage({ type: "page-ready"}, "*");
+        
+        count += 1000;
+        if (count >= 10000) {
+            var data = { type: "FROM_PAGE", text: "Unable to Connected"};
+            window.postMessage(data, "*");
+            clearInterval(interval);
+        }
+    
+    }, 1000); 
+    
+    function afterHandshake() {
+
+        if (!conected) return;
+
+        var data = { type: "FROM_PAGE", text: "V2 running"};
+        window.postMessage(data, "*");
+
+        // code here
+        
+
+    } // afterHandshake function
+
+    function formSubmit() {
+        // function for after you submit the hijacked one
+        var data = { type: "FROM_PAGE", text: "Form submit activated"};
+        window.postMessage(data, "*");
+    }
+    
+})();
