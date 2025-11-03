86676a4613e8efe55b19ce58894521f6927ca037
cartieraidan <cartieraidan@gmail.com>
2025-11-03 15:26:28 -0500

    Update injectTest.js

--- injectTest.js
+++ injectTest.js
@@ -1,5 +1,41 @@
 (function () {
 
+    function injectButton() {
+        const button = document.createElement('button');
+
+        button.textContent = "CLICK";
+        button.id = "InjectedButton";
+        button.classList.add("my-custom-button");
+
+        Object.assign(button.style, {
+            width: '200px',
+            height: '50px',
+            background: 'teal',
+            color: 'white',
+            fontSize: '20px',
+            cursor: 'pointer',
+            zIndex: 2147483647
+        });
+
+        const container = document.getElementById('thread-bottom-container');
+
+        //if (container) {
+           // container.appendChild(button);
+       //} else {
+        //    document.body.appendChild(button);
+       // }
+
+        document.body.appendChild(button);
 
 
- })();
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', injectButton);
+    } else {
+        injectButton();
+    }
+    
+
+
+})();
