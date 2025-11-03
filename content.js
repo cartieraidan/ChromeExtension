b7056e5c25fe97be85df5fb7e91cdc66e346d49c
cartieraidan <cartieraidan@gmail.com>
2025-11-03 15:26:51 -0500

    Update content.js

--- content.js
+++ content.js
@@ -19,44 +19,12 @@ window.addEventListener("message", function(event) {
 
 });
 
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('injectTest.js');
-script.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(script); // loads script into browser
+{
+  const scriptInject = document.createElement('script');
+  scriptInject.src = chrome.runtime.getURL('injectTest.js');
+  scriptInject.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(scriptInject); // loads script into browser
 
-
-//const button = document.getElementById("composer-submit-button");
-//const form = button.closest("form");
-// for replacing button
-/*
-if (button) {
-  chrome.runtime.sendMessage({ type: "log", message: "Removing button"});
-
-  const computedStyle = window.getComputedStyle(button);
-
-  const newButton = document.createElement("button");
-  for (let prop of computedStyle) {
-    newButton.style[prop] = computedStyle.getPropertyValue(prop);
-  }
-
-  
-  newButton.innerHTML = button.innerHTML;
-
-  newButton.id = "my-submit-button";
-  newButton.type = "button";
-  
-
-  //function for button here
-  newButton.addEventListener("click", () => {
-    window.open("https://google.com", "_blank");
-    //send a http request from here
-    //also need to another function for stoping submit forms with the enter key
-  })
-  
-  button.parentNode.insertBefore(newButton, button.nextSibling);
-  button.style.display = "none";
-  chrome.runtime.sendMessage({ type: "log", message: "Button Replaced"});
 }
-*/
