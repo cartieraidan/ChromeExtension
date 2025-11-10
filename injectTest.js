7b18127aef5800071ca38a617aae28af88808e72
cartieraidan <cartieraidan@gmail.com>
2025-11-09 20:46:20 -0500

    changing style of button

--- injectTest.js
+++ injectTest.js
@@ -8,9 +8,9 @@
         button.classList.add("my-custom-button");
 
         Object.assign(button.style, {
-            width: '200px',
-            height: '50px',
-            background: 'teal',
+            width: '100px',
+            height: '100px',
+            background: 'rgba(35, 141, 36, 1)',
             color: 'white',
             fontSize: '20px',
             cursor: 'pointer',
@@ -19,7 +19,9 @@
             bottom: '20px',
             right: '20px',
             border: 'none',
-            borderRadius: '8px'
+            borderRadius: '50px',
+            boxShadow: '0 0 15px 3px rgba(24, 94, 32, 0.6)', 
+            transition: 'box-shadow 0.3s ease'
         });
 
         document.body.appendChild(button);
