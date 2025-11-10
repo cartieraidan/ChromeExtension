3027c38648ab1e9686ca071b8e4d9ef363447912
cartieraidan <cartieraidan@gmail.com>
2025-11-09 20:54:18 -0500

    trimming fat

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ document.getElementById("myButton").addEventListener("click", () => {
     
     chrome.runtime.sendMessage({ type: "log", message: "button has been pressed" });
 
-
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            files: ["content.js"]
-        });
-    });
 });
